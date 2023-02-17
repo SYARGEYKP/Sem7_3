b858a01a577b369b98d8b01eec56c12fb8293d7e
SYARGEY <lss1895@mail.ru>
2023-02-17 20:31:07 +0300

    Семинар 3, задание 3

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage} > ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int[,] GenerateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array) // печатаем массив
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+void Average(int[,] array, int rows, int columns)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            sum += array[i, j];
+        }
+        double avg = sum / rows;
+        System.Console.Write($"{avg:f1}\t");
+    }
+}
+int r = InputInt("введите количество строк");
+int c = InputInt("введите количество столбцов");
+System.Console.WriteLine();
+int[,] matrix = GenerateArray(r, c);
+PrintArray(matrix);
+Average(matrix,r,c);
+
+
+
